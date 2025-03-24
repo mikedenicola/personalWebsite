962d68e2bbc23f076eb39fbc05049b541b6beeae
Michael Denicola <denicolamichael99@gmail.com>
2025-03-24 12:19:43 -0500

    Style changes and added scroll to top feature

--- personal-website/src/app/landing-page/landing-page.component.ts
+++ personal-website/src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, PLATFORM_ID, viewChild, ViewChild } from '@angular/core';
 import { FooterComponent } from '../shared-components/footer/footer.component';
 import { HeaderComponent } from '../shared-components/header/header.component';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 
 @Component({
@@ -10,6 +11,49 @@ import { HeaderComponent } from '../shared-components/header/header.component';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-}
+
+  private scrollButton: HTMLElement | undefined;
+  @ViewChild('homeContainer') scrollContainer!: ElementRef;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.scrollButton = this.document.querySelector('.scroll-button') as HTMLElement;
+      this.scrollContainer.nativeElement.addEventListener('scroll', () => {
+        this.handleScroll();
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.scrollContainer.nativeElement.removeEventListener('scroll', this.handleScroll);
+    };
+  };
+
+   handleScroll = (): void => {
+    const scrollPosition = this.scrollContainer.nativeElement.scrollTop;
+    if (scrollPosition > 50) {
+      this.scrollButton?.classList.add('visible');
+    } else {
+      this.scrollButton?.classList.remove('visible');
+    };
+  };
+
+  scrollToTop() {
+    if (this.scrollContainer?.nativeElement) {
+      this.scrollContainer.nativeElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+  };
+};
