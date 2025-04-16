59282c6ee3f08548caee3dca17057d4c04086f24
Michael Denicola <denicolamichael99@gmail.com>
2025-04-16 12:58:53 -0500

    update header and add button functionality

--- personal-website/src/app/landing-page/landing-page.component.ts
+++ personal-website/src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,24 @@
-import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, PLATFORM_ID, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
-
-
   private scrollButton: HTMLElement | undefined;
   @ViewChild('homeContainer') scrollContainer!: ElementRef;
 
@@ -24,7 +31,9 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.scrollButton = this.document.querySelector('.scroll-button') as HTMLElement;
+      this.scrollButton = this.document.querySelector(
+        '.scroll-button'
+      ) as HTMLElement;
       this.scrollContainer.nativeElement.addEventListener('scroll', () => {
         this.handleScroll();
       });
@@ -33,25 +42,42 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.scrollContainer.nativeElement.removeEventListener('scroll', this.handleScroll);
-    };
-  };
+      this.scrollContainer.nativeElement.removeEventListener(
+        'scroll',
+        this.handleScroll
+      );
+    }
+  }
 
-   handleScroll = (): void => {
+  handleScroll = (): void => {
     const scrollPosition = this.scrollContainer.nativeElement.scrollTop;
     if (scrollPosition > 50) {
       this.scrollButton?.classList.add('visible');
     } else {
       this.scrollButton?.classList.remove('visible');
-    };
+    }
   };
 
   scrollToTop() {
     if (this.scrollContainer?.nativeElement) {
       this.scrollContainer.nativeElement.scrollTo({
         top: 0,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
-    };
-  };
-};
+    }
+  }
+
+  jumpToAbout() {
+    const introSection = this.document.getElementById('Intro');
+    introSection?.scrollIntoView({
+      behavior: 'smooth', block: "end"
+    });
+  }
+
+  jumpToProjects() {
+    const projectSection = this.document.getElementById('Projects');
+    projectSection?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+}
