d973069d8e862e7051f517f91f265739f1fe6ce0
Michael Denicola <denicolamichael99@gmail.com>
2025-04-21 16:42:13 -0500

    update main content and styling

--- personal-website/src/app/app.routes.ts
+++ personal-website/src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: '', component: LandingPageComponent, title: 'Home page' },
-  { path: '**', component: PageNotFoundComponent } // Wildcard route for 404 errors
-];
+  { path: '', component: LandingPageComponent, title: 'Home page' }];
--- personal-website/src/app/landing-page/landing-page.component.ts
+++ personal-website/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,32 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, CommonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss',
+  styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
+
+export class LandingPageComponent implements OnDestroy, AfterViewInit {
+
   private scrollButton: HTMLElement | undefined;
+  private autoSlideInterval!: ReturnType<typeof setInterval>;
+  public hoverPhone: boolean = false;
+  public hoverEmail: boolean = false;
+  public currentIndex: number = 0;
+  public techStackImages: string[] = [
+    'assets/angular-logo.png',
+    'assets/ts-logo.png',
+    'assets/icons8-javascript.svg',
+    'assets/aws-logo.png',
+    'assets/icons8-bootstrap.svg',
+    'assets/icons8-java.svg',
+    'assets/icons8-node-js.svg',
+    'assets/icons8-postgresql.svg',
+  ];
+
   @ViewChild('homeContainer') scrollContainer!: ElementRef;
   @ViewChild('tooltip') tooltip: MatTooltip | undefined;
 
@@ -18,24 +35,31 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
     @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
-  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
+      this.startAutoSlide();
       this.scrollButton = this.document.querySelector('.scroll-button') as HTMLElement;
       this.scrollContainer.nativeElement.addEventListener('scroll', () => {
         this.handleScroll();
+      });
+
+      const highlight = document.querySelector('.cursor-highlight') as HTMLElement;
+      document.addEventListener('mousemove', (e) => {
+        highlight!.style.top = `${e.clientY}px`;
+        highlight!.style.left = `${e.clientX}px`;
       });
     };
   }
 
   ngOnDestroy(): void {
     if (isPlatformBrowser(this.platformId)) {
+      this.stopAutoSlide();
       this.scrollContainer.nativeElement.removeEventListener(
         'scroll',
         this.handleScroll
       );
-    };
+    }
   }
 
   handleScroll = (): void => {
@@ -44,7 +68,7 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
       this.scrollButton?.classList.add('visible');
     } else {
       this.scrollButton?.classList.remove('visible');
-    };
+    }
   };
 
   scrollToTop() {
@@ -53,7 +77,7 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
         top: 0,
         behavior: 'smooth',
       });
-    };
+    }
   }
 
   jumpToAbout() {
@@ -64,13 +88,27 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
     });
   }
 
-  //update name
-  jumpToProjects() {
-    const projectSection = this.document.getElementById('Projects');
+  jumpToStack() {
+    const projectSection = this.document.getElementById('Stack');
     projectSection?.scrollIntoView({
       behavior: 'smooth',
     });
   }
+
+  jumpToExperience() {
+    const projectSection = this.document.getElementById('experience');
+    projectSection?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
+  jumpToContact() {
+    const projectSection = this.document.getElementById('footer');
+    projectSection?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
 
   copyText(id: string, tooltip: MatTooltip) {
     const textToCopy = this.document.getElementById(id)!.textContent || '';
@@ -78,7 +116,42 @@ export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
       tooltip.disabled = false;
       tooltip.show();
       setTimeout(() => tooltip.hide(), 1000);
-      setTimeout(() => tooltip.disabled = true, 1100);
+      setTimeout(() => (tooltip.disabled = true), 1100);
     });
+  }
+
+
+  getPositionClass(index: number): string {
+    const total = this.techStackImages.length;
+    const offset = (index - this.currentIndex + total) % total;
+
+    if (offset === 0) return 'center';
+    if (offset <= 2) return `right-${offset}`;
+    if (offset >= total - 2) return `left-${total - offset}`;
+    return '';
+  }
+
+  nextSlide() {
+    this.stopAutoSlide();
+    this.currentIndex = (this.currentIndex + 1) % this.techStackImages.length;
+    this.startAutoSlide();
+  }
+
+  prevSlide() {
+    this.stopAutoSlide();
+    this.currentIndex = (this.currentIndex - 1 + this.techStackImages.length) % this.techStackImages.length;
+    this.startAutoSlide();
+  }
+
+  startAutoSlide(intervalMs: number = 10000): void {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextSlide();
+    }, intervalMs);
+  }
+
+  stopAutoSlide(): void {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+    };
   }
 }
