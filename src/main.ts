d7d95ed4fc5373cc2e73cc4e9a52a45183622ed0
Michael Denicola <denicolamichael99@gmail.com>
2026-02-05 08:11:01 -0600

    Fix file structure

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync()],
+  providers: [provideRouter(routes, withComponentInputBinding())],
 })
   .catch((err) => console.error(err));
